93a34210e9b4e1eeb5bdeadcd629eee5836ed065
nandagenu <nandafmgenu@gmail.com>
2024-09-26 17:00:12 -0300

    passando parâmetros

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,10 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/', [SobreNosController::class, 'sobrenos']);
 
 Route::get('/', [ContatoController::class, 'contato']);
+
+Route::get(
+      '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+      function(string $nome, string $categoria, string $assunto, string $mensagem) {
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+      }
+);
