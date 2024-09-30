1ea9a727750fdd144cb1ff6608c6506ce2b592a8
nandagenu <nandafmgenu@gmail.com>
2024-09-30 14:41:28 -0300

    criando menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,7 @@ use App\Http\Controllers\ContatoController;
 
 Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/sobrenos', [SobreNosController::class, 'sobrenos']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobrenos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
 
-Route::get(
-      '/contato/{nome}/{categoria_id}',
-      function(
-        string $nome = 'Desconhecido', 
-        int $categoria_id = 1
-        ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-      }
-)->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
