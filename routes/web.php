1ebc5c19a9d58697b496759f8dfdad61110c14f6
nandagenu <nandafmgenu@gmail.com>
2024-10-02 16:07:53 -0300

    estruturas de controle @if/@else

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\TesteController;
+use App\Http\Controllers\FornecedorController;
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
@@ -13,7 +14,7 @@ Route::get('/login', function() {return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
   Route::get('/clientes', function() {return 'Clientes';})->name('app.clientes');
-  Route::get('/fornecedores', function() {return 'Fornecedores';})->name('app.fornecedores');
+  Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
   Route::get('/produtos', function() {return 'Produtos';})->name('app.produtos');
 });
 
