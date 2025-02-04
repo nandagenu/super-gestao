ca0f1760272bec039106a8f51de95b45f4b4be82
Fernanda Genú <nandafmgenu@gmail.com>
2025-02-04 17:03:30 -0300

    components e @csrf

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\FornecedorController;
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function() {return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
