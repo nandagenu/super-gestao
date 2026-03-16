ce74e54cc21509bfd97a0b72d162c84e71539386
nandagenu <nandafmgenu@gmail.com>
2026-03-16 17:24:56 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return "Olá, seja bem-vindo.";
+});
+
+Route::get('/sobre-nos', function () {
+    return "Sobre nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
+});
