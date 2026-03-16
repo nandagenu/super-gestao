c5b85dcc8b6d56a6bab8d04492c3911b1ab013c8
nandagenu <nandafmgenu@gmail.com>
2026-03-16 17:58:53 -0300

    conexão rota-controller iniciais

---
+++ app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function sobreNos() {
+        echo "Sobre nós";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
-Route::get('/', function () {
-    return "Olá, seja bem-vindo.";
-});
+Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function () {
-    return "Sobre nós";
-});
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato', function () {
-    return "Contato";
-});
+Route::get('/contato', [ContatoController::class, 'contato']);
