7b66fc4936095842d08f59e895762f236b13a0bd
nandagenu <nandafmgenu@gmail.com>
2026-03-18 18:42:36 -0300

    aprimorando rotas e views

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,13 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login';})->name('site.login');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos';})->name('app.produtos');
+});
